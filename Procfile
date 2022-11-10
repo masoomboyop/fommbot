81900c3e4fb3d37890deef0dcde0f02c7c4ab4be
zσηєуℓσвєℓу <93111779+Chiranjibkoch@users.noreply.github.com>
2022-11-10 16:10:15 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m fommbot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE fomm bot
+:: Enables virtual env mode and then starts Fallen
+env\scripts\activate.bat && py -m fommbot
